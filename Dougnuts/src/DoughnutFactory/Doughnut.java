3c94e0ed27c6edfa5fc73619156a056367b41868
Drlinx <Dragonlinkx@gmail.com>
2023-04-18 11:34:56 -0600

    commented doughtnut class

--- Dougnuts/src/DoughnutFactory/Doughnut.java
+++ Dougnuts/src/DoughnutFactory/Doughnut.java
@@ -12,10 +12,18 @@ public class Doughnut {
     float Cost;
     int count;
 
+    /**
+     * @brief constructor class
+     * @param St Style of doughnut
+     * @param Cat Catagory the doughnut falls under
+     * @param c Cost of the doughnut
+     * @param x quantity of doughnuts; This is temporary.
+     */
     Doughnut(String St, String Cat, float c, int x){
         Style = St;
         catagory = Cat;
         Cost = c;
+        count = x;
     }
 
 }
