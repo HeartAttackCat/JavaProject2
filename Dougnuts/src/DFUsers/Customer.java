c559b3e0a5018aec2f4f3c50a4cdacfcaab3323e
Drlinx <Dragonlinkx@gmail.com>
2023-04-18 12:32:46 -0600

    Organized project into packages/folders

---
+++ Dougnuts/src/DFUsers/Customer.java
@@ -0,0 +1,24 @@
+/**
+ * @author simplex
+ * @file App.java
+ * @assignment Project 2
+ * @brief Class for user interactions by a customer.
+ */
+package DFUsers;
+
+public class Customer extends User{
+
+    /**
+     * @brief constructor class for customer.
+     */
+    Customer(){
+        super();
+    }
+
+    /**
+     * @brief prints out the entire menu for the user.
+     */
+    void ViewMenu(){
+
+    }
+}
---
+++ Dougnuts/src/DFUsers/Employee.java
@@ -0,0 +1,32 @@
+/**
+ * @author simplex
+ * @file Employee.java
+ * @assignment Project 2
+ * @brief User class for actions the employee can do.
+ */
+package DFUsers;
+
+public class Employee extends User {
+
+    /**
+     * @brief constructor for employee.
+     */
+    Employee(){
+        super();
+    }
+
+    /**
+     * @brief shows the employee all pending orders.
+     */
+    void ViewOrders(){
+
+    }
+
+    /**
+     * @brief Updates an existing pending orders process to finished.
+     * @return 0 if successful
+     */
+    int UpdateOrder(){
+        return 0;
+    }
+}
