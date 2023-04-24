2ca4bb6784ef9d550f30c04cc2445dea122ad40a
Drlinx <Dragonlinkx@gmail.com>
2023-04-24 09:34:01 -0600

    Began working on invetory constructor

--- Dougnuts/src/DoughnutFactory/Inventory.java
+++ Dougnuts/src/DoughnutFactory/Inventory.java
@@ -7,19 +7,50 @@
 
 package DoughnutFactory;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Inventory {
-    ArrayList<DoughnutStack> inv = new ArrayList<DoughnutStack>();
+    ArrayList<DoughnutStack> ginv = new ArrayList<DoughnutStack>();
+    ArrayList<DoughnutStack> binv = new ArrayList<DoughnutStack>();
 
     /**
      * @brief constructor class
      *        TODO:
      *        Read from inventory file and initalize
-     *        check if expired | Yes? skip this line | No? add into arraylist
+     *        check if expired  | Yes? Add to bad; date = 0-0-0  only used for generating the 
+     *                                 expired report.
+     *                          | No? add to good; 
      */
     public Inventory() {
-        
+        String str;
+        String catagory;
+        String type;
+        String date;
+        int quant;
+        Doughnut temp; 
+        Scanner fp;
+
+        // Loads the file
+        try {
+            fp = new Scanner(new File("./menu.csv"));
+            System.out.println(fp.nextLine());
+            System.out.println(fp.hasNextLine());
+            while (fp.hasNextLine()) {
+                str = fp.nextLine();
+                System.out.print(str);
+                catagory = str.split(",", 4)[0];
+                type = str.split(",", 4)[1];
+                date = str.split(",", 4)[2];
+                quant = Integer.parseInt(str.split(",", 4)[3]);
+                
+                
+            }
+            fp.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } 
     }
 
     public int newitem() {
