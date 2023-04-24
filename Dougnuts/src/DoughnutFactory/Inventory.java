4b14596e2804d1a10dd3200c10431022a0213899
Drlinx <dragonlinkx@gmail.com>
2023-04-24 10:40:17 -0600

    Forgot semicolon

--- Dougnuts/src/DoughnutFactory/Inventory.java
+++ Dougnuts/src/DoughnutFactory/Inventory.java
@@ -49,7 +49,7 @@ public class Inventory {
                 type = str.split(",", 4)[1];
                 date = str.split(",", 4)[2];
                 quant = Integer.parseInt(str.split(",", 4)[3]);
-                price = Float.parseFloat(str.split(",", 4)[4])
+                price = Float.parseFloat(str.split(",", 4)[4]);
 
                 year = Integer.parseInt(date.split("-", 3)[0]);
                 month = Integer.parseInt(date.split("-", 3)[1]);
